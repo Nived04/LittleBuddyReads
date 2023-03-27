18bb92fcb0400a74fdb6919c2a7f1780fa94d216
Nived04 <119923686+Nived04@users.noreply.github.com>
2023-03-27 23:36:42 +0530

    Delete pages directory

--- pages/about.php
+++
@@ -1,2 +0,0 @@
-<?php require_once($_SERVER["DOCUMENT_ROOT"].'/nived/LittleBud3/templates/header.php'); ?>
-<?php require_once($_SERVER["DOCUMENT_ROOT"].'/nived/LittleBud3/templates/footer.php'); ?>
