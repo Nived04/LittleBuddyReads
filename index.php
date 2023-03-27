41fd64fb2aeed9bf868b2f1a85274680fc6fb712
Nived04 <119923686+Nived04@users.noreply.github.com>
2023-03-27 23:05:53 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php require_once('templates/header.php'); ?>
+<div class="container">
+
+    <div class="starter-template">
+    	<h1>Welcome to Little Buddy Reads</h1>
+		<div id="slideShowImages">
+		    <img src="images/download2.jpg" style="width: 90%; height: auto; " alt="Slide 1" />
+		    <img src="images/download3.jpg" style="width: 90%; height: auto; " alt="Slide 2" />
+		    <img src="images/images.jpg" style="width: 90%; height: auto; " alt="Slide 3" />    
+		    <img src="images/images(1).jpg" style="width: 90%; height: auto; " alt="Slide 4" />
+  		</div>
+    </div>
+</div><!-- /.container -->
+    <!-- <img src="images/download2.jpg" border=4 width=500 height="800" align="left">    
+    	<img src="images/download3.jpg" border=4 width=500 height="800" align="right">
+    	<img src="images/images.jpg" border=4 width=400 height="300" align="centre"><br><br>   
+    	<img src="images/images(1).jpg" border=4 width=400 height="300" align="centre">
+    	<br/> -->
+   <?php require_once('templates/footer.php'); ?>
---
+++ templates/footer.php
@@ -0,0 +1,15 @@
+	<footer class="footer">
+      <div class="container">
+        <p class="text-muted">Made By Nived Shah's Company Pvt. Ltd. &copy;</p>
+      </div>
+    </footer>
+<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"><\/script>')</script>
+	<script src="/bootstrap/js/bootstrap.min.js"></script>
+	<script src="http://samples.msdn.microsoft.com/workshop/samples/imageslideshow/slideshow.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+  </body>
+</html>
