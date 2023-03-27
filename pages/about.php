983772dd9bc11ead0a0b6bc06acb1a9015ce1173
Nived04 <119923686+Nived04@users.noreply.github.com>
2023-03-27 23:40:40 +0530

    Add files via upload

---
+++ pages/about.php
@@ -0,0 +1,2 @@
+<?php require_once($_SERVER["DOCUMENT_ROOT"].'/nived/LittleBud3/templates/header.php'); ?>
+<?php require_once($_SERVER["DOCUMENT_ROOT"].'/nived/LittleBud3/templates/footer.php'); ?>
